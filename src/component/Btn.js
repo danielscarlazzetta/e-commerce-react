0c4e0c774f34c1888a788f44121e94b676a2a425
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2024-07-11 18:27:04 -0400

    ajuste btn en el cart

---
+++ src/component/Btn.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Btn.css';
+
+function Btn({text}) {
+  return (
+      <button class="btn-cart">{text}</button>
+  );
+}
+
+export default Btn;
--- src/component/Cart.js
+++ src/component/Cart.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import './Cart.css';
+import React from "react";
+import "./Cart.css";
+import Btn from "./Btn";
 
 function Cart({ isOpen, onClose }) {
   return (
-    <div className={`cart ${isOpen ? 'open' : ''}`}>
-      <button className="close-btn" onClick={onClose}>X</button>
-      <h2>Carrito de Compras</h2>
-      <ul>
-        <li>Producto 1</li>
-        <li>Producto 2</li>
-        <li>Producto 3</li>
-      </ul>
-    </div>
+    <>
+      <div className={`cart ${isOpen ? "open" : ""}`}>
+        <button className="close-btn" onClick={onClose}>
+          X
+        </button>
+        <h2>Carrito de Compras</h2>
+        <ul>
+          <li>Producto 1</li>
+          <li>Producto 2</li>
+          <li>Producto 3</li>
+        </ul>
+      <Btn text="Realizar compra"/>
+      </div>
+    </>
   );
 }
 
